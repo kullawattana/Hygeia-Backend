a603bb8a4aab85c1fee3ac5895ef9252583e0316
suttipong kullawattana <suttipong.kull@gmail.com>
2019-04-17 22:57:36 +0700

    First Commit
    First Commit

---
+++ htdocs/MyApi/includes/Constants.php
@@ -0,0 +1,12 @@
+<?php
+
+    //DATABASE CONNECTION
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASSWORD', '');
+    define('DB_NAME', 'my_app');
+
+    //DATABASE STATUS
+    define('USER_CREATED', 101);
+    define('USER_EXISTS', 102);
+    define('USER_FAILURE', 103);
